e267e574a73b065aa475a81f7432622c3040cc93
Robert Young <ryoung@tresensa.com>
2022-10-30 20:55:15 -0300

    Added react paper native

--- reactor/App.js
+++ reactor/App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { Searchbar } from 'react-native-paper';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
+
   return (
+    <PaperProvider>
     <View style={styles.container}>
+    <Searchbar
+    style={styles.searchbar}
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
       <Text>Test app.</Text>
+
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  searchbar: {
+    marginTop: 15,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
