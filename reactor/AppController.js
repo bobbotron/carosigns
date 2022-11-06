98fb28173353de7ee7949fa1ca4134876693ff7f
Rob Young <robert.young@vungle.com>
2022-11-06 18:15:50 -0500

    Added basic search.

--- reactor/AppController.js
+++ reactor/AppController.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Searchbar } from "react-native-paper";
+import { Button, Searchbar } from "react-native-paper";
 import { useEffect, useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import MainList from "./components/MainList";
 import Level from "./components/Level";
 import SignsDB from "./data/SignDb";
+import SignDetail from "./components/SignDetail";
 
 export default function AppController() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -20,7 +21,7 @@ export default function AppController() {
   const searchActive = !(searchQuery === undefined || searchQuery === "");
   const selectedSigns = !searchActive
     ? undefined
-    : SignsDB.Signs.filter((x) => x.name.includes(searchQuery));
+    : SignsDB.Signs.filter((x) => x.name.toLowerCase().includes(searchQuery));
 
   const levelListener = (l) => {
     setSelectedLevel(l);
@@ -52,10 +53,27 @@ export default function AppController() {
 
       {searchActive && (
         <>
-          <Text>
-            Hey there's a search happening! {selectedSigns.length} results
-            found.
-          </Text>
+          {selectedSigns.length === 0 && <Text>No results found.</Text>}
+          {selectedSigns.length === 1 && (
+            <View style={{ marginTop: 15 }}>
+              <SignDetail sign={selectedSigns[0]} />
+            </View>
+          )}
+          {selectedSigns.length > 1 && (
+            <>
+              <Text>{selectedSigns.length} results found.</Text>
+              {selectedSigns.map((x) => (
+                <Button
+                  key={x.name}
+                  mode="contained"
+                  style={{ marginTop: 10 }}
+                  onPress={() => setSearchQuery(x.name)}
+                >
+                  {x.name}
+                </Button>
+              ))}
+            </>
+          )}
         </>
       )}
 
