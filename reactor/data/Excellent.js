1611a10473a8d36ff101e17252a7d0ab50eb4393
Aus-Paws <117685763+Aus-Paws@users.noreply.github.com>
2022-11-07 16:01:52 -0500

    Added sign data

---
+++ reactor/data/Excellent.js
@@ -0,0 +1,82 @@
+const Excellent =
+
+  [{
+    name: "300",
+    icon: require("../assets/slides/300.jpg"),
+    category: "Excellent",
+    description: undefined
+  }, {
+    name: "301",
+    icon: require("../assets/slides/301.jpg"),
+    category: "Excellent",
+    description: undefined
+  }, {
+    name: "302",
+    icon: require("../assets/slides/302.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "303",
+    icon: require("../assets/slides/303.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "304",
+    icon: require("../assets/slides/304.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "305",
+    icon: require("../assets/slides/305.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "306",
+    icon: require("../assets/slides/306.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "307",
+    icon: require("../assets/slides/307.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "308",
+    icon: require("../assets/slides/308.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "309",
+    icon: require("../assets/slides/309.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "310",
+    icon: require("../assets/slides/310.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "311",
+    icon: require("../assets/slides/311.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "312",
+    icon: require("../assets/slides/312.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "313",
+    icon: require("../assets/slides/313.jpg"),
+    category: "Excellent",
+    description: undefined
+  },{
+    name: "314",
+    icon: require("../assets/slides/314.jpg"),
+    category: "Excellent",
+    description: undefined
+  },
+
+  ];
+
+export default Excellent;
