7a6e3920a849e413f0678d984e3b682d15eb07ec
Heather Hunter <heather.p.hunter@gmail.com>
2022-11-20 14:48:41 -0500

    Added sign decriptions and deductions for 200-206

--- reactor/data/Advanced.js
+++ reactor/data/Advanced.js
@@ -1,112 +1,329 @@
-const Advanced =
-
-  [{
+const Advanced = [
+  {
     name: "200",
     icon: require("../assets/slides/200.jpg"),
     category: "Advanced",
-    description: undefined
-  }, {
+    title: "200. HALT – Fast From Sit",
+    description:
+      "• The team comes to a <b>halt</b> and <b>the dog sits in heel position.</b><br/>• The handler cues the dog to heel and moves <b> immediately into a fast pace from the halt</b>.<br/>• This station is followed by either Slow, Normal or the Finish station.<br/><br/>",
+    tips: "• The Judge is looking for the <i>dog</i> to noticeably change their pace. A jogging pace from the handler is recommended.<br/>• The fast pace must begin immediately from the sit.<br/>",
+    deductions: {
+      minor: [
+        "Dog sits more than 45-degrees out of heel position",
+        "Handler moves their feet during the stationary portion of the halt",
+        "Dog out of heel position",
+      ],
+      nq: [
+        "Dog sits 90-degrees or more out of heel position",
+        "Dog and Handler do not move immediately into fast pace from the sit",
+        "Dog does not noticeably change their pace",
+        "Failure of the dog to assume a cued position",
+        "Dog assumes a position not required by the exercise",
+      ],
+    },
+    video: undefined,
+  },
+  {
     name: "201",
     icon: require("../assets/slides/201.jpg"),
     category: "Advanced",
-    description: undefined
-  }, {
+    title: "201. HALT – Leave Dog - Call to Heel while Running",
+    description:
+      "• The team comes to a <b>halt</b> and <b>the dog sits in heel position.</b><br/>• The handler cues the <b>dog to stay</b> and moves forward at a <b>fast pace</b>.<br/> • <b>After the handler has completely passed the 5-foot line</b>, they call the dog to heel as they return to normal pace.<br/>• The handler may pause or stop once they have returned to normal pace and after passing the 5-foot line.<b/><br/>• The dog must hold the sit stay until called<b/>.<br/><br/>",
+    tips: "• The handler must move at a <i>fast (ie. jogging or running)</i> pace for <i>at least</i> 5 feet. You can run beyond the 5 foot line to be sure you do not slow down too soon. If so, be sure your dog has enough time to return to heel position before the next station sign.<br/> • If you are physically unable to run from a stand still, request an accommodation at the time of entry.<br/>",
+    deductions: {
+      minor: [
+        "Dog sits more than 45-degrees out of heel position",
+        "Handler moves their feet during the stationary portion of the halt",
+        "Dog out of heel position",
+      ],
+      nq: [
+        "Dog sits 90-degrees or more out of heel position",
+        "Handler does not move immediately into fast pace from the halt",
+        "Handler does not maintain the fast pace for at least 5 feet (until crossing the line)",
+        "Dog does not hold the sit stay while the handler runs (minor foot movement is ok)",
+        "Failure of the dog to assume a cued position",
+        "Dog assumes a position not required by the exercise",
+      ],
+    },
+    video: undefined,
+  },
+  {
     name: "202",
     icon: require("../assets/slides/202.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "202. Dog Circles Left",
+    description:
+      "• While heeling the <b>dog spins in a circle in place to the left</b> beside the handler.<br/>• The handler may pause briefly as needed.<br/><br/>",
+    tips: "• A 'pause' does allow for a full stop of forward motion, however, you may incur a minor deduction if the pause is not interpreted by the judge as brief. The dog must not sit.<br/>• As with position changes, you may use a physical and a verbal cue simultaneously for the spin without incurring a deduction for a second cue.<br/>",
+    deductions: {
+      minor: [
+        "Dog out of heel position",
+        "Handler's pause (stop of forward motion) is not brief",
+        "Dog stops forward motion",
+      ],
+      nq: [
+        "Dog does not perform the spin or spins in the incorrect direction",
+        "Dog assumes a position not required by the exercise",
+      ],
+    },
+    video: undefined,
+  },
+  {
     name: "203",
     icon: require("../assets/slides/203.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "203. Moving Down, Walk Around",
+    description:
+      "• While heeling, the handler cues the <b>dog to down and without pausing walks around the dog from head to tail</b> returning to heel position.<br>• The handler remains within arm’s length of the dog throughout the exercise.<br/>• The handler may choose to pause upon returning to heel position before heeling away from the station.<br/>• The handler cues the dog to <b>heel forward from the down position</b><br/><br/>",
+    tips: "• The handler's path should flow smoothly and without interruption from heel position while walking around the dog.<br/>• The judge is looking for the dog to have at least <i>begun</i> the down by the time you cross in front of their paws (no deductions).<br/>• If the dog does not <i>complete</i> the down until you have returned to heel position, you will incur a substantial deduction for promptness, but can still meet the requirements of the exercise.<br/>• You may <i>slightly</i> slow your pace as you circle the dog so that they have time to complete the down but significantly slowing your pace is a substantial deduction<br/>• Walk a tight circle around the dog.<br/>",
+    reward: true,
+    deductions: {
+      minor: [
+        "Down more than 45-degrees out of heel position",
+        "Handler is further than arm's length during the walk around",
+        "Dog still in the process of completing the down when the handler passes by their tail (2 points)",
+        "Reinforcement (dropped): dog leaves position to go to food that has been dropped (2 points)",
+      ],
+
+      substantial: [
+        "Handler significantly slows their pace during the moving down",
+        "Failure of the dog to assume the down promptly",
+      ],
+
+      nq: [
+        "Down 90-degrees or more out of heel position",
+        "Dog turns with the handler rather than performing a down",
+        "Handler stops forward motion while performing the moving down",
+        "Dog has not <i>begun<i/>the down by the time the handler passes their far shoulder",
+        "Dog does not hold the down for the duration of the walk around and until cued to heel forward",
+        "Dog assumes a position not required by the exercise",
+        "Reinforcement: handler puts their hand into the pocket where the food is located before the dog has completed the station, uses the food as a lure, or stores the food in their left pocket",
+      ],
+    },
+    video: undefined,
+  },
+  {
     name: "204",
     icon: require("../assets/slides/204.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "204. Moving Side Step Right",
+    description:
+      "• While heeling, the handler takes <b>one step sideways to their right</b> while cueing the dog to heel.<br/>• The dog <b>must move sideways to the right simultaneously with the handler</b>, remaining in heel position parallel to the handler.<br/>• The team heels forward along the newly established line.<br/><br/>",
+    tips: "• The team will heel directly up to the station sign and perform the side step in front of the sign, then proceed along the new path with the sign to their left.<br/>• Ideally the dog's body position should remain completely parallel to the handler throughout.<br/>",
+    deductions: {
+      minor: [
+        "During the side step, the dog’s shoulder remains in heel position but their body is at 45-degree angle (2 points)",
+        "Dog out of heel position",
+        "Dog stops forward motion",
+      ],
+      nq: [
+        "The dog does not move simultaneously with the handler",
+        "During the side step, the dog’s shoulders are in heel position but their body is more than 45-degrees out of position",
+        "Dog assumes a position not required by the exercise",
+      ],
+    },
+    video: undefined,
+  },
+  {
     name: "205",
     icon: require("../assets/slides/205.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "205. Off-set Figure 8",
+    description:
+      "• Two cones (pylons) and two food bowls are arranged in a diamond pattern:<br/> o The cones are placed 10 feet apart,<br/> o The food bowls are securely covered and are placed 2.5 feet from the centre line.<br/>• The team enters the pattern and <b>completes a continuous Figure 8 around the pylons, crossing through the middle at least three times</b>.<br/>• The team <b>exits the exercise as indicated by the placement of the next station.</b><br/>• Allowances can be made in the distance between the food bowls for large/giant breeds.<br/><br/>",
+    tips: "• Unlike other cone exercises, you will not always enter with the first cone on the dog's left. The course map should indicate the intended entry and exit points by arrows.<br/>• A cue such as 'leave it' can be used without deductions if it is given before the dog leaves heel position. Cues must never be given in a corrective tone.<br/>• In the Brace class, dogs will have one extra foot between the cones and between the bowls.<br/>",
+    deductions: {
+      minor: ["Out of heel position", "Dog sniffs the bowls (2 points)", "Minor movement (bump) of a cone (no deduction if a cone is moved by a wagging tail)", "Handler or dog stop forward motion"],
+      substantial: ["Dog leaves another station to sniff the bowls","Knocking over or substantial movement of a cone"],
+      nq: ["The dog knocks over the food bowls, tips them, bites at them or moves them out of position."],
+    },
+  },
+  {
     name: "206",
     icon: require("../assets/slides/206.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "206. Left Turn Dog Circles Right",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "207",
     icon: require("../assets/slides/207.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "207. Left About Turn",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "208",
     icon: require("../assets/slides/208.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "208. HALT - 180 Degree Pivot Right - HALT",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    reward: true,
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "209",
     icon: require("../assets/slides/209.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "209. HALT - 180-Degree Pivot Left - HALT",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    reward: true,
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "210",
     icon: require("../assets/slides/210.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "210. HALT - From Sit - 180 right",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "211",
     icon: require("../assets/slides/211.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "211. HALT - From Sit - 180 left",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "212",
     icon: require("../assets/slides/212.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "212. HALT - Stand – Leave for Walk Around",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "213",
     icon: require("../assets/slides/213.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "213. Return & Forward from Stand",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    reward: true,
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "214",
     icon: require("../assets/slides/214.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "214. HALT - Leave Dog – Turn and Call Front",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "215",
     icon: require("../assets/slides/215.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "215. Call Front - 1, 2, 3 Steps Backward",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "216",
     icon: require("../assets/slides/216.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "216. Finish Right",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    reward: true,
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "217",
     icon: require("../assets/slides/217.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "217. Finish Left",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    reward: true,
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "218",
     icon: require("../assets/slides/218.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "218. Forward Right",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "219",
     icon: require("../assets/slides/219.jpg"),
     category: "Advanced",
-    description: undefined
-  },{
+    title: "219. Forward Left",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
+  },
+  {
     name: "220",
     icon: require("../assets/slides/220.jpg"),
     category: "Advanced",
-    description: undefined
+    title: "220. Send over Jump",
+    description: "• .<br/><br/>",
+    tips: "• .<br/>",
+    deductions: {
+      minor: ["", "", ""],
+      nq: ["", "", ""],
+    },
   },
-
-  ];
+];
 
 export default Advanced;
