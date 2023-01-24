d830ab8582cef7d66a2263acdf8affbf93e280dc
Rob Young <bobbotron@gmail.com>
2023-01-23 19:51:18 -0500

    Initial commit

---
+++ reactor/data/Working.js
@@ -0,0 +1,33 @@
+const Working = [
+  {
+    name: "1",
+    icon: undefined, // the sign
+    category: "Working",
+    title: "1. Drop on recall",
+    levels: {
+      rookie: {
+        procedure: "Woah",
+        procedureImages: [require("../assets/slides/200.jpg")],
+        layout: "",
+        layoutImages: [],
+        passRequirements: [],
+      },
+      elite: {
+        procedure: "Woah",
+        procedureImages: [],
+        layout: "",
+        layoutImages: [],
+        passRequirements: [],
+      },
+      expert: {
+        procedure: "Woah",
+        procedureImages: [],
+        layout: "",
+        layoutImages: [],
+        passRequirements: [],
+      },
+    },
+  },
+];
+
+export default Working;
