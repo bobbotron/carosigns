58c1d91f431e92c56c54640d59fadbddc4f8383d
Heather Hunter <heather.p.hunter@gmail.com>
2023-01-27 20:44:46 -0500

    Entered test data into Working DOR

--- reactor/data/Working.js
+++ reactor/data/Working.js
@@ -1,19 +1,19 @@
-const Working = { name: "Working",
+const Working = { name: "Working Stream",
 type: "working",
 buttonColor: "blue",
 signs: [
   {
-    name: "1",
-    icon: require("../assets/slides/start.jpg"), // Todo update this!
-    category: "Working",
-    title: "1. Drop on recall",
+    name: "DOR",
+    icon: require("../assets/slides/DOR.png"),
+    category: "Working Stream",
+    title: "1. Drop on Recall",
     levels: {
       rookie: {
-        procedure: "Woah woah",
-        procedureImages: [require("../assets/slides/200.jpg")],
+        procedure: "The exercise begins when the handler steps into the dog box.<br/>• The handler cues the dog to sit in heel position,<br/>• Cues the dog to stay, proceeds to the handler box and turns to face the dog,<br/>• Calls the dog and then cues the following sequence:<br/>▪	Down between the 10-foot and 15-foot lines,<br/>▪ Call to front position.<br/><br/>The exercise ends when the dog assumes front position",
+        procedureImages: [],
         layout: "",
-        layoutImages: [],
-        passRequirements: [],
+        layoutImages: [require("../assets/slides/200.jpg")],
+        passRequirements: ["The dog starts in heel position,", "Stays in position(s) until cued to move,","Moves directly towards the handler until cued to perform another behaviour,","Has front paws within the designated zone when assuming a position,","Comes to front position."],
       },
       elite: {
         procedure: "Woah",
