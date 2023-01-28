b9ebf200731f0b60e34aec7e5e9b85942730a67c
Heather Hunter <heather.p.hunter@gmail.com>
2023-01-28 15:57:22 -0500

    Entered DOR layout graphic

--- reactor/data/Working.js
+++ reactor/data/Working.js
@@ -5,7 +5,7 @@ signs: [
   {
     name: "DOR",
     icon: require("../assets/slides/DOR.png"),
-    category: "Working Stream",
+    category: "Working Exercises",
     title: "1. Drop on Recall",
     
     levels: {
@@ -13,15 +13,15 @@ signs: [
         procedure: "The exercise begins when the handler steps into the dog box.<br/>• The handler cues the dog to sit in heel position,<br/>• Cues the dog to stay, proceeds to the handler box and turns to face the dog,<br/>• Calls the dog and then cues the following sequence:<br/>▪	Down between the 10-foot and 15-foot lines,<br/>▪ Call to front position.<br/>The exercise ends when the dog assumes front position.<br/><br/>",
         procedureImages: [],
         layout: "",
-        layoutImages: [require("../assets/slides/200.jpg")],
+        layoutImages: [require("../assets/LayoutDiag/w1_Rookie.png")],
         passRequirements: ["The dog starts in heel position,", "Stays in position(s) until cued to move,","Moves directly towards the handler until cued to perform another behaviour,","Has front paws within the designated zone when assuming a position,","Comes to front position."],
       },
       elite: {
-        procedure: "Woah",
+        procedure: "The exercise begins when the handler steps into the dog box.<br/>• The handler cues the dog to sit in heel position,<br/>• Cues the dog to stay, proceeds to the handler box and turns to face the dog,<br/>• Calls the dog and then cues the following sequence:<br/>▪	Down between the 10-foot and 15-foot lines,<br/>▪ Call to front position.<br/>The exercise ends when the dog assumes front position.<br/><br/>",
         procedureImages: [],
         layout: "",
-        layoutImages: [],
-        passRequirements: [],
+        layoutImages: [require("../assets/LayoutDiag/w1_Elite.png")],
+        passRequirements: ["The dog starts in heel position,", "Stays in position(s) until cued to move,","Moves directly towards the handler until cued to perform another behaviour,","Has front paws within the designated zone when assuming a position,","Comes to front position."],
       },
       expert: {
         procedure: "Woah",
